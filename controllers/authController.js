50b68ed736512dd6caabc84e53845e65a44cfac5
Kamikaze92 <mrxatob@gmail.com>
2021-11-16 23:49:26 +0700

    adding register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,39 @@
+const { User } = require("../models");
+
 class AuthController {
-    static async login (req,res,next){
-        try {
-            
-        } catch (err) {
-            
-        }
+  static async login(req, res, next) {
+    try {
+    } catch (err) {}
+  }
+  static async register(req, res, next) {
+    try {
+      let { username, email, password } = req.body;
+      const created = await User.create({
+        username,
+        email,
+        password,
+      });
+      res
+        .status(201)
+        .json({
+          id: created.id,
+          username: created.username,
+          email: created.email,
+        });
+    } catch (err) {
+      console.log(err);
+      switch (err.name) {
+        case "SequelizeValidationError":
+        case "SequelizeUniqueConstraintError":
+          res.status(400).json(err.errors[0].message);
+          break;
+
+        default:
+          res.status(500).json(err.message);
+          break;
+      }
     }
-    static async register (req,res,next){
-        try {
-            
-        } catch (err) {
-            
-        }
-    }
+  }
 }
 
-module.exports = AuthController
+module.exports = AuthController;
