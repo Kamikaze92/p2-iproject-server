d36b938a9bac9937746119d3f621d6a822d781bf
Kamikaze92 <mrxatob@gmail.com>
2021-11-17 00:13:47 +0700

    add login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,41 @@
+const { comparePassword } = require('../helpers/bcrypt');
+const { signToken } = require('../helpers/jwt')
 const { User } = require("../models");
-
 class AuthController {
   static async login(req, res, next) {
     try {
-    } catch (err) {}
+      const { email, password } = req.body;
+      if(!email || !password){
+          throw {name: "empty input"}
+      }
+      const user = await User.findOne({
+        where: {
+          email: email,
+        },
+      });
+      if (!user || !comparePassword(password, user.password)) {
+        //username:fy@gmail.com  password:farhad
+        throw { name: "invalid input" };
+      } else {
+        let tokenUser = { id: user.id, email: user.email, username: user.username };
+        let access_token = signToken(tokenUser);
+        res.status(200).json({ id: user.id, email: user.email, username: user.username, access_token });
+      }
+    } catch (err) {
+      console.log(err);
+      switch (err.name) {
+        case "invalid input":
+          res.status(400).json({message: "invalid email/password"});
+          break;
+        case "empty input":
+        res.status(400).json({message: "fill all the field required"});
+        break;
+
+        default:
+          res.status(500).json(err.message);
+          break;
+      }
+    }
   }
   static async register(req, res, next) {
     try {
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,11 +1,11 @@
-var jwt = require('jsonwebtoken');
+const jwt = require ('jsonwebtoken');
 
 function signToken(access_token){
-    return jwt.sign(access_token, process.env.JWT_SIGNATURE);
+    return jwt.sign(access_token, process.env.JWT_CODE);
 }
 
 function verifyToken(access_token){
-    return jwt.verify(access_token, process.env.JWT_SIGNATURE);
+    return jwt.verify(access_token, process.env.JWT_CODE);
 }
 
 module.exports = {signToken, verifyToken}
