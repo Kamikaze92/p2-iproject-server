3b9fe84b951597bf69094b54c9ebf7dfa7ac2b71
Kamikaze92 <mrxatob@gmail.com>
2021-11-18 09:37:49 +0700

    deploy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require('cors');
 const router = require('./router');
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({extended:false}))
 app.use(cors())
